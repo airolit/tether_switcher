b3aa4f2d41f9c4404fc4b8ce367eb256499ce8b0
Philip K. Gisslow <ripxorip@gmail.com>
2022-03-01 13:23:35 +0100

    Initial commit

---
+++ set_tether.py
@@ -0,0 +1,73 @@
+"""
+Example code of how to read and write vehicle parameters using pymavlink
+"""
+
+from __future__ import print_function
+
+import time, struct, os
+
+# Import mavutil
+from pymavlink import mavutil
+
+def mavset(mav, name, value, retries=3, parm_type=None):
+    '''set a parameter on a mavlink connection'''
+    print("Updating parameter: " + name)
+    got_ack = False
+
+    if parm_type is not None and parm_type != mavutil.mavlink.MAV_PARAM_TYPE_REAL32:
+        # need to encode as a float for sending
+        if parm_type == mavutil.mavlink.MAV_PARAM_TYPE_UINT8:
+            vstr = struct.pack(">xxxB", int(value))
+        elif parm_type == mavutil.mavlink.MAV_PARAM_TYPE_INT8:
+            vstr = struct.pack(">xxxb", int(value))
+        elif parm_type == mavutil.mavlink.MAV_PARAM_TYPE_UINT16:
+            vstr = struct.pack(">xxH", int(value))
+        elif parm_type == mavutil.mavlink.MAV_PARAM_TYPE_INT16:
+            vstr = struct.pack(">xxh", int(value))
+        elif parm_type == mavutil.mavlink.MAV_PARAM_TYPE_UINT32:
+            vstr = struct.pack(">I", int(value))
+        elif parm_type == mavutil.mavlink.MAV_PARAM_TYPE_INT32:
+            vstr = struct.pack(">i", int(value))
+        else:
+            print("can't send %s of type %u" % (name, parm_type))
+            return False
+        vfloat, = struct.unpack(">f", vstr)
+    else:
+        vfloat = float(value)
+
+    while retries > 0 and not got_ack:
+        retries -= 1
+        mav.param_set_send(name.upper(), vfloat, parm_type=parm_type)
+        tstart = time.time()
+        while time.time() - tstart < 1:
+            ack = mav.recv_match(type='PARAM_VALUE', blocking=False)
+            if ack is None:
+                time.sleep(0.1)
+                continue
+            if str(name).upper() == str(ack.param_id).upper():
+                got_ack = True
+                break
+    if not got_ack:
+        print("timeout setting %s to %f" % (name, vfloat))
+        return False
+    return True
+
+# Create the connection
+print("Connecting to drone..")
+master = mavutil.mavlink_connection("/dev/ttyACM0", baud=115200)
+# Wait a heartbeat before sending commands
+master.wait_heartbeat()
+
+# Set parameters
+mavset(master, 'MNT_MODE_IN', 2, parm_type=mavutil.mavlink.MAV_PARAM_TYPE_INT32)
+
+# Reboot before launching QGC
+print("Rebooting drone")
+master.reboot_autopilot()
+time.sleep(2)
+master.close()
+
+print("Launcing AGC in tether mode..")
+# Set a fake home for tether
+os.environ["HOME"] = "/home/ripxorip/tmp/tether_home"
+os.system("/home/ripxorip/dev/airolitgroundcontrol/build_x86/debug/staging/qgroundcontrol-start.sh")
